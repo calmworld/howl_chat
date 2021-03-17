d03cde249fb6b94cfce6787fc412e356689b7d19
Sarah <sarah.alhuriz@hotmail.com>
2021-03-16 21:37:03 -0600

    add componenets to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import Channels from './Components/Channels';
+import Chat from './Components/Chat';
+import Store from './store';
+import Form from './Components/Form'
+import Auth from './Components/Auth'
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      
+      <Store> {/* main context */}
+      <Auth> {/* basic auth/welcome component */}
+      <Header/>
+      <div className="main-wrapper">
+      <Channels />
+      <Chat  /> {/* component rendering msg's */}
+      </div>
+      <Form/> {/* msg input component */}
+        </Auth>
+      </Store>
     </div>
   );
 }
