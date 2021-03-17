57cf0a5c9f3176be515c093452b46e46baa44adb
Sarah <sarah.alhuriz@hotmail.com>
2021-03-16 22:26:32 -0600

    client is ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import './App.css';
-import Channels from './Components/Channels';
-import Chat from './Components/Chat';
-import Store from './store';
-import Form from './Components/Form'
-import Auth from './Components/Auth'
-import Header from './Components/Header';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./index.css";
+import Home from "./Home/Home";
+import ChatRoom from "./ChatRoom/ChatRoom";
 
 function App() {
   return (
-    <div>
-      <Store> {/* main context */}
-      <Auth> {/* basic auth/welcome component */}
-      <Header/>
-      <div className="main-wrapper">
-      <Channels />
-      <Chat  /> {/* component rendering msg's */}
-      </div>
-      <Form/> {/* msg input component */}
-        </Auth>
-      </Store>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:roomId" component={ChatRoom} />
+      </Switch>
+    </Router>
   );
 }
 
